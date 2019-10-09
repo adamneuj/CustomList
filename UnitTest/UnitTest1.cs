5c9e00fd0c7c25901c4e19b486e67aa7b12faba1
Adam Neujahr <adamneujahr@gmail.com>
2019-10-09 09:39:25 -0500

    Created unitTest project that references the customlist class.

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -8,8 +8,35 @@ namespace UnitTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Add_AddToEmptyList_ItemGoesToIndexZero()
         {
+            // arrange
+            CustomList<int> testList = new CustomList<int>();
+            int expected = 12;
+            int actual;
+
+            // act
+            testList.Add(12);
+            actual = testList[0];
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_AddItemToList_CountIncrements()
+        {
+            // arrange
+            CustomList<int> testList = new CustomList<int>();
+            int expected = 1;
+            int actual;
+
+            // act
+            testList.Add(234);
+            actual = testList.Count;
+
+            // assert
+            Assert.AreEqual(expected, actual);
         }
     }
 }

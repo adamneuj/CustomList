73ee930cec2dec3a81865b606fe3042260f87ca0
Adam Neujahr <adamneujahr@gmail.com>
2019-10-09 14:33:33 -0500

    Added count property.

--- CustomList/CustomList.cs
+++ CustomList/CustomList.cs
@@ -9,11 +9,21 @@ namespace CustomList
     public class CustomList<T>
     {
         private T[] items;
+        private int count;
 
         //constructor
         public CustomList()
         {
             items = new T[4];
+        }
+
+        public int Count
+        {
+            get
+            {
+                count = items.Length;
+                return count;
+            }
         }
     }
 }
